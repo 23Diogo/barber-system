c99cb7914792322a5ba5d54938e2ebd8f43f86ca
23Diogo <129696135+23Diogo@users.noreply.github.com>
2026-04-15 10:28:39 -0300

    Update subscriptions.routes.ts

--- src/modules/subscriptions/subscriptions.routes.ts
+++ src/modules/subscriptions/subscriptions.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express'
 import { authenticate } from '../../middleware/auth'
 import { checkLicense } from '../../middleware/license'
-import { list, getOne, create, update, generateNextCycle, consume } from './subscriptions.controller'
+import {
+  list,
+  getOne,
+  getActiveByClient,
+  create,
+  update,
+  generateNextCycle,
+  consume,
+} from './subscriptions.controller'
 
 const router = Router()
 router.use(authenticate, checkLicense)
 
 router.get('/', list)
+router.get('/client/:clientId/active', getActiveByClient)
 router.get('/:id', getOne)
 router.post('/', create)
 router.patch('/:id', update)
