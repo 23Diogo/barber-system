32617a2ea4564315cbee3d9aabe1b9cb294f3c23
23Diogo <129696135+23Diogo@users.noreply.github.com>
2026-04-16 14:38:17 -0300

    Update client-auth.routes.ts

--- src/modules/client-auth/client-auth.routes.ts
+++ src/modules/client-auth/client-auth.routes.ts
@@ -1,1 +1,19 @@
-//teste
+import { Router } from 'express'
+import {
+  registerClient,
+  loginClient,
+  meClient,
+  forgotPasswordClient,
+  resetPasswordClient,
+} from './client-auth.controller'
+import { authenticateClient } from '../../middleware/client-auth'
+
+const router = Router()
+
+router.post('/register', registerClient)
+router.post('/login', loginClient)
+router.get('/me', authenticateClient, meClient)
+router.post('/forgot-password', forgotPasswordClient)
+router.post('/reset-password', resetPasswordClient)
+
+export default router
